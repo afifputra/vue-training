f0beadd17c8f3819300eb60690c470e83f5583b8
afifputra <afifputraharyono@gmail.com>
2022-06-15 21:59:53 +0700

    Task 1 : Data Binding

--- 2. Basic Vue/basics-assignment-1-problem/app.js
+++ 2. Basic Vue/basics-assignment-1-problem/app.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
     return {
       myName: "Afif Putra Haryono",
       myAge: 23,
-      myImage: "https://api.duniagames.co.id/api/content/upload/file/8755136771648463695.jpg",
+      myImage: "https://i0.wp.com/mrtunakarya.com/wp-content/uploads/2022/01/WhatsApp-Image-2022-01-25-at-09.33.51-3.jpeg?resize=680%2C382&ssl=1",
       myFavoriteNumber: Math.random(),
     };
   },
