73adf2313fc0895e96e528f351685b4c2e7ad79b
afifputra <afifputraharyono@gmail.com>
2022-06-16 06:11:03 +0700

    end event section

--- 2. Basic Vue/basics-03-events-starting-code/app.js
+++ 2. Basic Vue/basics-03-events-starting-code/app.js
@@ -1,9 +1,29 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      name: "",
+      confirmationName: "",
     };
+  },
+  methods: {
+    increment(number) {
+      this.counter += number;
+    },
+    decrement(number) {
+      this.counter -= number;
+    },
+    setName(event, lastName) {
+      lastName = "Putra";
+      this.name = `${event.target.value} ${lastName}`;
+    },
+    confirmName() {
+      this.confirmationName = this.name;
+    },
+    submitForm() {
+      alert("Submitted!");
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
