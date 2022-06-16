18160bead7deaee058a613b0792b2d81c1d9eb49
afifputra <afifputraharyono@gmail.com>
2022-06-16 15:35:30 +0700

    update user can insert css when enter on first element

--- 2. Basic Vue/basics-assignment-4-problem/app.js
+++ 2. Basic Vue/basics-assignment-4-problem/app.js
@@ -3,28 +3,35 @@ const app = Vue.createApp({
     return {
       userInput: "",
       hiddenParagraph: false,
+      finalUserInput: "",
       inputBackgroundColor: "",
     };
   },
   computed: {
     message() {
-      const result = this.userInput === "user1" || this.userInput === "user2" ? "Applied!" : "Style me!";
+      console.log("computed message");
+      const result = this.finalUserInput === "user1" || this.finalUserInput === "user2" ? "Applied!" : "Style me!";
       return result;
     },
     changeClassCss() {
+      console.log("changeClassCss");
       if (this.hiddenParagraph) {
-        return `hidden ${this.userInput}`;
+        return `hidden ${this.finalUserInput}`;
       } else {
-        return `visible ${this.userInput}`;
+        return `visible ${this.finalUserInput}`;
       }
     },
     changeBackgroundColor() {
+      console.log("changeBackgroundColor");
       return { backgroundColor: this.inputBackgroundColor };
     },
   },
   methods: {
     getDataInput(event) {
       this.userInput = event.target.value;
+    },
+    setFinalUserInput() {
+      this.finalUserInput = this.userInput;
     },
     changeHiddenParagraph() {
       this.hiddenParagraph = !this.hiddenParagraph;
