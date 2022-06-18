7c4322c0302079eb0cc9c787820853e47c7a5a58
afifputra <afifputraharyono@gmail.com>
2022-06-18 09:16:09 +0700

    communicate with component

---
+++ 7. Workflow With Vue-CLI/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        { id: "John", name: "John Max", phone: "088765432123", email: "john@mailinator.com" },
+        { id: "Jane", name: "Jane Doe", phone: "088765432123", email: "jane@mailinator.com" },
+      ],
+    };
+  },
+};
+</script>
---
+++ 7. Workflow With Vue-CLI/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: { id: "John", name: "John Max", phone: "088765432123", email: "john@mailinator.com" },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
