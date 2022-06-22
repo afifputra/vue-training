6cfbe924888e1510ed2d0ba16ca453acf6a15dac
afifputra <afifputraharyono@gmail.com>
2022-06-22 17:15:55 +0700

    using slot

--- 9. Diving Deeper Into Component/cmp-adv-01-starting-setup/src/main.js
+++ 9. Diving Deeper Into Component/cmp-adv-01-starting-setup/src/main.js
@@ -2,11 +2,13 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import BaseBadge from "./components/BaseBadge.vue";
+import BaseCard from "./components/BaseCard.vue";
 import UserInfo from "./components/UserInfo.vue";
 
 const app = createApp(App);
 
 app.component("base-badge", BaseBadge);
+app.component("base-card", BaseCard);
 app.component("user-info", UserInfo);
 
 app.mount("#app");
