c16d53ef930a7df9756d255cbe3123c293c61778
afifputra <afifputraharyono@gmail.com>
2022-06-23 06:02:18 +0700

    manage error

--- 9. Diving Deeper Into Component/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ 9. Diving Deeper Into Component/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,4 +1,36 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text" name="tes" id="tes" />
+  <input type="text" name="tes" id="tes" ref="goal" />
+  <button @click="setGoal">Submit</button>
+  <error-alert v-if="isError">
+    <h2>Input is empty</h2>
+    <p>Please enter a goal</p>
+    <button @click="closeError">Okay!</button>
+  </error-alert>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      isError: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredGoal = this.$refs.goal.value;
+      if (enteredGoal === "") {
+        this.isError = true;
+        return;
+      }
+    },
+    closeError() {
+      this.isError = false;
+    },
+  },
+};
+</script>
