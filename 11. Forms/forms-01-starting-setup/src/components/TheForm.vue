65bcebe9cb9e30dc7f4718f5e720f0d10feb6874
afifputra <afifputraharyono@gmail.com>
2022-06-25 12:47:20 +0700

    working with form

--- 11. Forms/forms-01-starting-setup/src/components/TheForm.vue
+++ 11. Forms/forms-01-starting-setup/src/components/TheForm.vue
@@ -1,16 +1,16 @@
 <template>
-  <form>
+  <form @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model="userName" />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model="userAge" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="refferer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -51,6 +51,28 @@
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userAge: null,
+      refferer: 'wom',
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(`Username: ${this.userName}`);
+      console.log(`Age: ${this.userAge}`);
+      console.log(`Referrer: ${this.refferer}`);
+      this.userName = '';
+      this.userAge = null;
+      this.refferer = 'wom';
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
