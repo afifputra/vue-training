010aaadb05245d444d68a524740550b48666899b
afifputra <afifputraharyono@gmail.com>
2022-07-10 20:19:02 +0700

    animating 2 direct child element

--- 14. Animation Transition/animations-01-starting-setup/src/App.vue
+++ 14. Animation Transition/animations-01-starting-setup/src/App.vue
@@ -9,6 +9,12 @@
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
   <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -25,6 +31,7 @@ export default {
       dialogIsVisible: false,
       isAnimating: false,
       paraIsVisible: false,
+      usersAreVisible: false,
     };
   },
   computed: {
@@ -46,6 +53,12 @@ export default {
     },
     toggleParagraph() {
       this.paraIsVisible = !this.paraIsVisible;
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
     },
   },
 };
@@ -95,6 +108,24 @@ button:active {
 
 .animated {
   animation: movingToLeft 0.3s ease-out forwards;
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
 }
 
 /* .v-enter-from {
