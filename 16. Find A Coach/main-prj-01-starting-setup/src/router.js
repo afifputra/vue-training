c41e6a17cb24ce7acc13aa12dc09bea2d7ba8313
afifputra <afifputraharyono@gmail.com>
2022-08-05 14:47:32 +0700

    initial commit add authenticate

--- 16. Find A Coach/main-prj-01-starting-setup/src/router.js
+++ 16. Find A Coach/main-prj-01-starting-setup/src/router.js
@@ -5,6 +5,7 @@ import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestReceived from './pages/requests/RequestReceived.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
@@ -36,6 +37,10 @@ const router = createRouter({
     {
       path: '/request',
       component: RequestReceived,
+    },
+    {
+      path: '/auth',
+      component: UserAuth,
     },
     {
       path: '/:notFound(.*)',
