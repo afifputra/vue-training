cb0e28176ae712c58c2a9499b49abb4e12cacd8d
afifputra <afifputraharyono@gmail.com>
2022-08-05 16:28:26 +0700

    add auth

--- 16. Find A Coach/main-prj-01-starting-setup/src/pages/auth/UserAuth.vue
+++ 16. Find A Coach/main-prj-01-starting-setup/src/pages/auth/UserAuth.vue
@@ -1,22 +1,70 @@
 <template>
   <base-card>
-    <form>
+    <form @submit.prevent="onSubmit">
       <div class="form-control">
         <label for="email">Email</label>
-        <input type="email" name="email" id="email" />
+        <input type="email" name="email" id="email" v-model.trim="email" />
       </div>
       <div class="form-control">
         <label for="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          v-model.trim="password"
+        />
       </div>
-      <base-button>Login</base-button>
-      <base-button type="button" mode="flat">Signup instead</base-button>
+      <p style="color: red" v-if="!formIsValid">
+        Incorrect email or password. Please try again. At least 6 characters.
+      </p>
+      <base-button>{{ captionSubmit }}</base-button>
+      <base-button type="button" mode="flat" @click="switchMode">{{
+        captionSwitch
+      }}</base-button>
     </form>
   </base-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login',
+    };
+  },
+  computed: {
+    captionSubmit() {
+      return this.mode === 'login' ? 'Login' : 'Signup';
+    },
+    captionSwitch() {
+      return this.mode === 'login' ? 'Signup instead' : 'Login instead';
+    },
+  },
+  methods: {
+    onSubmit() {
+      this.formIsValid = true;
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.password.length < 6
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+      console.log(this.email, this.password, this.mode);
+    },
+    switchMode() {
+      if (this.email !== '' || this.password !== '') {
+        this.email = '';
+        this.password = '';
+      }
+      this.mode = this.mode === 'login' ? 'signup' : 'login';
+    },
+  },
+};
 </script>
 
 <style scoped>
