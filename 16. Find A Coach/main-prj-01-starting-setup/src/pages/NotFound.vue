893ae50fcb0b3e9d719e1ec5baa1e4a80b7d8dae
afifputra <afifputraharyono@gmail.com>
2022-08-05 13:51:39 +0700

    make page not found

---
+++ 16. Find A Coach/main-prj-01-starting-setup/src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page Not Found</h2>
+      <p>
+        This page can't be found - maybe check all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
