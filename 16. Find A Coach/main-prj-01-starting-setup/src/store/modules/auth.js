2777cc8737ef918df005c52cab98eaeea9ff04bc
afifputra <afifputraharyono@gmail.com>
2022-08-07 19:14:58 +0700

    make auto login with local storage

--- 16. Find A Coach/main-prj-01-starting-setup/src/store/modules/auth.js
+++ 16. Find A Coach/main-prj-01-starting-setup/src/store/modules/auth.js
@@ -25,90 +25,66 @@ export default {
   },
   actions: {
     async login(context, payload) {
-      const response = await fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBMAS1HexE_4NN1LLm40tOXaBsSy7sDVzg',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: payload.email,
-            password: payload.password,
-            returnSecureToken: true,
-          }),
-        }
-      );
-      const responseData = await response.json();
-
-      if (!response.ok) {
-        console.log(responseData);
-        const error = new Error(responseData.error.message);
-        throw error;
-      }
-
-      console.log(responseData);
-      context.commit('setUser', {
-        token: responseData.idToken,
-        userId: responseData.localId,
-        tokenExpiration: responseData.expiresIn,
+      return context.dispatch('auth', {
+        ...payload,
+        mode: 'login',
       });
     },
+
     async signup(context, payload) {
-      const response = await fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBMAS1HexE_4NN1LLm40tOXaBsSy7sDVzg',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: payload.email,
-            password: payload.password,
-            returnSecureToken: true,
-          }),
-        }
-      );
+      return context.dispatch('auth', {
+        ...payload,
+        mode: 'signup',
+      });
+    },
+
+    async auth(context, payload) {
+      const mode = payload.mode;
+      let url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBMAS1HexE_4NN1LLm40tOXaBsSy7sDVzg';
+
+      if (mode === 'signup')
+        url =
+          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBMAS1HexE_4NN1LLm40tOXaBsSy7sDVzg';
+
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      });
       const responseData = await response.json();
 
       if (!response.ok) {
-        console.log(responseData);
         const error = new Error(responseData.error.message);
         throw error;
       }
 
-      console.log(responseData);
+      localStorage.setItem('token', responseData.idToken);
+      localStorage.setItem('userId', responseData.localId);
+
       context.commit('setUser', {
         token: responseData.idToken,
         userId: responseData.localId,
         tokenExpiration: responseData.expiresIn,
       });
     },
-    auth(context, payload) {
-        const mode = payload.mode;
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBMAS1HexE_4NN1LLm40tOXaBsSy7sDVzg';
 
-        if (mode === 'signup') url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBMAS1HexE_4NN1LLm40tOXaBsSy7sDVzg';
+    checkIsLoggedIn(context) {
+      const token = localStorage.getItem('token');
+      const userId = localStorage.getItem('userId');
 
-        const response = await fetch(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBMAS1HexE_4NN1LLm40tOXaBsSy7sDVzg',
-            {
-              method: 'POST',
-              body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true,
-              }),
-            }
-          );
-          const responseData = await response.json();
-    
-          if (!response.ok) {
-            console.log(responseData);
-            const error = new Error(responseData.error.message);
-            throw error;
-          }
-    
-          context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn,
-          });
+      if (token && userId) {
+        context.commit('setUser', {
+          token,
+          userId,
+          tokenExpiration: null,
+        });
+      }
     },
+
     logout(context) {
       context.commit('setUserLogout');
     },
