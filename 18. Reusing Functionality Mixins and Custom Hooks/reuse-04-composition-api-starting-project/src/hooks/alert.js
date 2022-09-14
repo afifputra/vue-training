ce68977a1c77959f64625c94dae7ed02c752290a
afifputra <afifputraharyono@gmail.com>
2022-09-14 16:07:57 +0700

    make hooks

---
+++ 18. Reusing Functionality Mixins and Custom Hooks/reuse-04-composition-api-starting-project/src/hooks/alert.js
@@ -0,0 +1,16 @@
+import { ref } from 'vue';
+
+const useAlert = () => {
+  const alertIsVisible = ref(false);
+
+  function showAlert() {
+    alertIsVisible.value = true;
+  }
+  function hideAlert() {
+    alertIsVisible.value = false;
+  }
+
+  return [alertIsVisible, showAlert, hideAlert];
+};
+
+export default useAlert;
